415021b0d50208e49059ce394e28f79ff73ad044
halfbro <mlagaren@stevens.edu>
2019-11-15 04:29:08 -0500

    Add basic bucket implementation

---
+++ kv_registry/lib/kv_registry/bucket.ex
@@ -0,0 +1,24 @@
+defmodule KVRegistry.Bucket do
+  @moduledoc """
+  A very basic implementation of a key value bucket that will be used
+  as a building block to build our KV registry.
+  """
+
+  use Agent
+
+  def new_bucket(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  def get(bucket, key) do
+    Agent.get(bucket, fn dict ->
+      Map.get(dict, key)
+    end)
+  end
+
+  def put(bucket, key, value) do
+    Agent.update(bucket, fn dict ->
+      Map.put(dict, key, value)
+    end)
+  end
+end
